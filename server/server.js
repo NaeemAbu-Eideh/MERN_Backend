00e2683f56586516059b58299000361e543f9912
NaeemAbu-Eideh <naeem.abueideh@gmail.com>
2025-12-29 06:47:41 +0200

    add ai

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 
 const connectDB = require("./config/db.config");
 const userRoutes = require("./routes/user.routes");
+const geminiRoutes = require("./routes/ai.route");
 
 const app = express();
 
@@ -12,10 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 app.use(userRoutes);
+app.use("/api/ai", geminiRoutes);
 
 connectDB(process.env.MONGO_URI);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(
     port,
